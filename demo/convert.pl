4f4b3eed35fcaae7f76793e1f4df1985cec6eea4
jinming <jinming1937@163.com>
2018-06-09 11:33:57 +0800

    convert.pl

--- demo/convert.pl
+++ demo/convert.pl
@@ -1,13 +1,20 @@
 # 摄氏度-华氏度 转换
 
 print "Enter a temperature (e.g., 32F, 100C):\n";
-$celsius = <STDIN>; # 从用户处接受一个输入
-chomp($celsius); # 去掉 $celsius 后的换行符
+$input = <STDIN>; # 从用户处接受一个输入
+chomp($input); # 去掉 $input 后的换行符
 
-if ( $celsius =~ m/^[+-]?\d+[CF]$/ ) {
-  $fahrenheit = ($celsius * 9 / 5) + 32; #计算华氏摄氏度
-  # print "$celsius C is $fahrenheit F.\n";
+if ( $input =~ m/^([+-]?\d+)([CF])$/ ) {
+  $InputNum = $1;
+  $type     = $2;
+  if ($type eq "C") {
+    $celsius = $InputNum;
+    $fahrenheit = ($celsius * 9 / 5) + 32; #计算华氏摄氏度
+  } else {
+    $fahrenheit = $InputNum;
+    $celsius = ($fahrenheit - 32 ) * 5 / 9; #计算华氏摄氏度
+  }
   printf "%.2f C is %.2f F.\n", $celsius, $fahrenheit; # printf 格式化输出
 } else {
-  print "Expecting a number, so I don't understand \"$celsius\".\n";
+  print "Expecting a number, so I don't understand \"$input\".\n";
 }
