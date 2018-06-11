ded1ee805234dbde805df4cb1513141b5ceb1705
jinming <jinming1937@163.com>
2018-06-12 01:00:31 +0800

    text to HTML

---
+++ lib/TextToHTML.pl
@@ -0,0 +1,32 @@
+undef $/;    # 进入“文件读取”模式
+$text = <>;  # 读入命令行中指定的第一个文件
+
+$text =~ $/&/&amp;/g;
+$text =~ $/</&lt;/g;
+$text =~ $/>/&gt;/g;
+
+$text =~ s/^\s*$/<p>/mg;
+
+# qr 生成一个正则表达式
+$HostnameRegex = qr/[-a-z0-9]+(\.[-a-z0-9]+)*\.(com|edu|info)/i;
+$text =~ s{
+  \b
+  (
+    \w[-.\w]*
+    \@
+    $HostnameRegex
+  )
+  \b
+}{<a href="mailto:$1">$1</a>}gix;
+
+$text =~ s{
+  \b
+  (
+    http:// $HostnameRegex \b
+    (
+      / [-a-z0-9_:\@&?=+,.!/~*'%\$]*
+      (?<![.,?!])
+    )?
+  )
+}{<a href="mailto:$1">$1</a>}gix;
+print $text;
--- replace/formatNum1.pl
+++ replace/formatNum1.pl
@@ -4,8 +4,8 @@
 #
 
 $num1 = 1234567890;
-$num1 =~ s/(?<=\d)(?=(\d\d\d)+$)/,/g;
-print "The US population is $num1\n";
+$num1 =~ s/(?<=\d)(?=(\d{3})+$)/,/g;
+print "The US population is 1: $num1\n";
 
 # 
 #
@@ -13,7 +13,7 @@ print "The US population is $num1\n";
 
 $num2 = 1234567890;
 $num2 =~ s/(?=(\d{3})+(?!\d))/,/g; # 注意这里要加g,
-print "The US population is $num2\n";
+print "The US population is 2: $num2\n";
 
 #
 #
@@ -21,19 +21,24 @@ print "The US population is $num2\n";
 
 $num3 = 1234567890;
 $num3 =~ s/(\d)(?=(\d{3})+(?!\d))/$1,/g; # 注意这里要加g,
-print "The US population is $num3\n";
+print "The US population is 3: $num3\n";
+
+$num3p = 1234567890;
+$num3p =~ s/(\d)(?=(\d{3})+$)/$1,/g; # X
+print "The US population is 3p: $num3p\n";
+
 
 #
-#
+# 至此，从右向左替换
 #
 
 $num4 = 1234567890;
-$num4 =~ s/(\d)(?=(\d{3})+\b)/$1,$2/g; # 注意这里要加g,
-print "The US population is $num4\n";
+$num4 =~ s/(\d)((\d{3})+\b)/$1,$2/g; # 注意这里要加g,
+print "The US population is 4: $num4\n";
 
 $num5 = 1234567890;
-while( $num5 =~ s/(\d)(?=(\d{3})+\b)/$1,$2/g ){
+while( $num5 =~ s/(\d)((\d{3})+\b)/$1,$2/g ){
   # no code
 }
-print "The US population is $num5\n";
+print "The US population is 5: $num5\n";
 
